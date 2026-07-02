19336d1a94fc2060eebcabbca883bb510998142e
Siddhika Gupta <siddhika22gupta@gmail.com>
2026-07-02 18:55:13 +0530

    feat(client):add authentication context

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,29 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+
+  const [user, setUser] = useState(null);
+
+  const [token, setToken] = useState(
+    localStorage.getItem("token") || null
+  );
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        setUser,
+        token,
+        setToken,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
